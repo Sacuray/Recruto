2fe5689da4e8234b9f5da6c96de528b13c0bb4d2
Sacuray <59845355+Sacuray@users.noreply.github.com>
2023-11-17 16:03:42 +0300

    Add files via upload

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 
 def index(request):
-    return HttpResponse("Hello " + request.GET["name"] + "! " + request.GET["message"].replace("%", " ") + "!")
+    a = random.randrange(1000, 9999, 1)
+    return HttpResponse("Hello your code is: " + str(a))
+
+
+def code(request):
+    a = random.randrange(1000, 9999, 1)
+    data = {"message": str(a)}
+    return render(request, "code1.html", context=data)
+
